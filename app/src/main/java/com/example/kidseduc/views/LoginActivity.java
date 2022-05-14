2936b1af40907f71ae1b516401d3460ce1a8b5a3
Zotoavina <Zotoavina.RASOAHARISOA@pulse.mg>
2022-05-14 13:25:14 +0200

    design login

--- app/src/main/java/com/example/kidseduc/views/LoginActivity.java
+++ app/src/main/java/com/example/kidseduc/views/LoginActivity.java
@@ -2,10 +2,15 @@ package com.example.kidseduc.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kidseduc.R;
 
@@ -18,6 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+        loginListener();
     }
 
     /**
@@ -27,4 +34,30 @@ public class LoginActivity extends AppCompatActivity {
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
     }
+
+    /**
+     * Ecoute évènement sur le boutton login
+     */
+    private void loginListener(){
+        ( (Button) findViewById(R.id.login)).setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // récupération des données saisies
+                String user = "";
+                String mdp = "";
+                try{
+                    user = username.getText().toString();
+                    mdp = password.getText().toString();
+                }catch (Exception ex){
+                    Toast.makeText(LoginActivity.this, "**************"+ex.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                if(user.equals("") || mdp.equals("")){
+                    Toast.makeText(LoginActivity.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "logged", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 }
