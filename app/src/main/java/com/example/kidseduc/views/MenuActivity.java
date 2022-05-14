21bbf41349284505a608f28505203aec39ba6bd8
Zotoavina <Zotoavina.RASOAHARISOA@pulse.mg>
2022-05-14 13:33:03 +0200

    ajout de l'activité menu

---
+++ app/src/main/java/com/example/kidseduc/views/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.kidseduc.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kidseduc.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
